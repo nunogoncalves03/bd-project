1041c8a2aae5c2841dc95957d20d9b98bcffc840
Nuno Gonçalves <nunomrgoncalves03@gmail.com>
2023-05-27 18:34:04 +0100

    Add check statements

--- delivery-2/schema.sql
+++ delivery-2/schema.sql
@@ -18,17 +18,17 @@ DROP TABLE IF EXISTS delivery CASCADE;
 CREATE TABLE customers (
     cust_no SERIAL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL CHECK (email LIKE '%@%.%'),
     phone TEXT NOT NULL,
     "address" TEXT
 );
 
 CREATE TABLE orders (
     order_no SERIAL PRIMARY KEY,
-    "date" DATE NOT NULL,
+    "date" DATE NOT NULL CHECK ("date" <= CURRENT_DATE),
     cust_no INTEGER NOT NULL,
     FOREIGN KEY (cust_no) REFERENCES customers (cust_no)
-    -- IC-1: any order_no in Order must exist in contains (implementable with trigger functions)
+    -- IC-1: any order_no in orders must exist in contains (implementable with trigger functions)
 );
 
 CREATE TABLE sales (
@@ -38,18 +38,18 @@ CREATE TABLE sales (
 
 CREATE TABLE pay (
     order_no INTEGER PRIMARY KEY,
-    cust_no INTEGER,
+    cust_no INTEGER NOT NULL,
     FOREIGN KEY (order_no) REFERENCES sales (order_no),
     FOREIGN KEY (cust_no) REFERENCES customers (cust_no)
-    -- IC-2: Customers can only pay for the Sale of an Order they have placed themselves
+    -- IC-2: customers can only pay for the sale of an order they have placed themselves
 );
 
 CREATE TABLE employees (
     ssn CHAR(11) PRIMARY KEY,
     tin CHAR(9) UNIQUE NOT NULL,
-    bdate DATE NOT NULL,
+    bdate DATE NOT NULL CHECK (bdate <= CURRENT_DATE),
     "name" TEXT NOT NULL
-    -- IC-3: any ssn in Employee must exist in works (implementable with trigger functions)
+    -- IC-3: any ssn in employees must exist in works (implementable with trigger functions)
 );
 
 CREATE TABLE process (
@@ -66,8 +66,8 @@ CREATE TABLE departments (
 
 CREATE TABLE workplaces (
     "address" TEXT PRIMARY KEY,
-    lat NUMERIC(10, 7) NOT NULL,
-    long NUMERIC(10, 7) NOT NULL,
+    lat NUMERIC(10, 7) NOT NULL CHECK (lat BETWEEN -90 AND 90),
+    long NUMERIC(10, 7) NOT NULL CHECK (long BETWEEN -180 AND 180),
     UNIQUE(lat, long)
 );
 
@@ -95,8 +95,8 @@ CREATE TABLE products (
     sku TEXT PRIMARY KEY,
     "name" TEXT NOT NULL,
     "description" TEXT,
-    price NUMERIC(10, 2) NOT NULL
-    -- IC-4: any sku in Product must exist in Supplier (implementable with trigger functions)
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0)
+    -- IC-4: any sku in products must exist in suppliers (implementable with trigger functions)
 );
 
 CREATE TABLE ean_products (
@@ -117,7 +117,7 @@ CREATE TABLE suppliers (
 CREATE TABLE contains (
     order_no INTEGER,
     sku TEXT,
-    qty INTEGER NOT NULL,
+    qty INTEGER NOT NULL CHECK (qty > 0),
     PRIMARY KEY (order_no, sku),
     FOREIGN KEY (order_no) REFERENCES orders (order_no),
     FOREIGN KEY (sku) REFERENCES products (sku)
